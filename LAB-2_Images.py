30350deb078f5cecd1613a8b70adf02d8b28f106
morgu <morgunov.96@bk.ru>
2023-10-04 12:55:08 +0300

    Model configured along with the graphics

---
+++ LAB-2_Images.py
@@ -0,0 +1,6 @@
+import tensorflow as tf
+
+from tensorflow.python.keras.layers import Dense, Flatten
+from tensorflow.python.keras import Model, Input
+import matplotlib.pyplot as plt
+import numpy as np
